34dc6233d0edfa4e8b4198f5f52862ae452faa52
hanmin0728 <gksalsdud1234@gmail.com>
2022-10-12 19:54:14 +0900

    카드 강조효과

--- Assets/01.Script/4.Minyoung/CardImage.cs
+++ Assets/01.Script/4.Minyoung/CardImage.cs
@@ -3,39 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using DG.Tweening;
 using UnityEngine.EventSystems;
 public class CardImage : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     private AbilitySO abilitySO;
 
-    public TextMeshProUGUI ablityNameText;
+    private RectTransform rect;
 
-    public Image profileImage;
+    public TextMeshProUGUI _ablityNameText;
+
+    public Image _profileImage;
 
     public TextMeshProUGUI _descTxt;
 
-    public Button _btn;
+    public Button _upgradeBtn;
 
+    private void Awake()
+    {
+        rect = GetComponent<RectTransform>();
+    }
     public void SetData(AbilitySO so)
     {
         abilitySO = so;
-        profileImage.sprite = so.sprite;
+        _profileImage.sprite = so.sprite;
         _descTxt.text = so.decString;
-        ablityNameText.text = "업그레이드 이히";
-        _btn.onClick.RemoveAllListeners();
-        _btn.onClick.AddListener(() => Debug.Log("눌림"));
-       // Debug.Log("셋데이타 잘실행됨");
+        _ablityNameText.text = "업그레이드 이히";
+        _upgradeBtn.onClick.RemoveAllListeners();
+        _upgradeBtn.onClick.AddListener(() => Debug.Log("눌림"));
+        // Debug.Log("셋데이타 잘실행됨");
     }
 
 
     public void OnPointerEnter(PointerEventData eventData)
     {
+        //eventData..DOScale(new Vector3(1.2f, 1.2f, 1.2f), 0.5);
+        rect.DOScale(new Vector2(1.2f, 1.2f), 0.5f);
+        rect.DOScaleX(1.2f, 0.3f);
+        rect.DOScaleY(1.2f, 0.3f);
         //강조효과
+
         Debug.Log("Enter");
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
+        rect.DOScale(Vector2.one, 0.5f);
         Debug.Log("Exit");
     }
 }
