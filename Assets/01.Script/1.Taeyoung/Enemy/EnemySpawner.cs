0397373260dd5b890e9d2cdf1152473e9bf001d9
LAPTOP_TAEYOUNG\69hop <l32590838@gmail.com>
2022-10-15 12:15:43 +0900

    Temp

--- Assets/01.Script/1.Taeyoung/Enemy/EnemySpawner.cs
+++ Assets/01.Script/1.Taeyoung/Enemy/EnemySpawner.cs
@@ -5,13 +5,31 @@ using System;
 
 public class EnemySpawner : MonoBehaviour
 {
-    void Start()
+    [SerializeField] private float[] spawnDelay;
+
+    private List<Transform> spawnPosList = new();
+    private int spawPosIndex;
+
+    private bool isCanSpawn = true;
+    public bool IsCanSpawn { get { return isCanSpawn; } set { isCanSpawn = value; } }
+
+    public void Start()
     {
-        
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            spawnPosList.Add(transform.GetChild(i));
+        }
+        StartCoroutine(SpawnSystem());
     }
 
-    void Update()
+    private IEnumerator SpawnSystem()
     {
-        
+        while (true)
+        {
+            yield return new WaitUntil(() => IsCanSpawn);
+            Debug.Log($"EnemySpawn At {spawnPosList[spawPosIndex].position}");
+            spawPosIndex = (spawPosIndex + 1) % spawnPosList.Count;
+            yield return new WaitForSeconds(spawnDelay[WaveManager.Instance.CurWave]);
+        }
     }
 }
