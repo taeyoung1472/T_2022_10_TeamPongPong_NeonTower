328fdb404faa31db75844222fd0564d926fbb27c
hanmin0728 <gksalsdud1234@gmail.com>
2022-10-16 01:16:31 +0900

    총알 패턴끝

--- Assets/01.Script/4.Minyoung/Enemy/Boss/BulletBoss.cs
+++ Assets/01.Script/4.Minyoung/Enemy/Boss/BulletBoss.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 public class BulletBoss : BossBase<BulletBoss>
 {
     public GameObject bullet;
-
+    public GameObject firecrackerBullet;
     private void Start()
     {
         bossFsm = new BossStateMachine<BulletBoss>(this, new BulletBossIdle());
+        //bossFsm = new BossStateMachine<BulletBoss>(this, new StraightBullet());
+        bossFsm.AddStateList(new CircleBullet());
         bossFsm.AddStateList(new StraightBullet());
+        bossFsm.AddStateList(new FirecrackerBullet());
     }
-    public void InstantiateObj(GameObject obj, Transform trm, Quaternion rot)
+    public GameObject InstantiateObj(GameObject obj, Transform trm, Quaternion rot)
     {
-        Instantiate(obj, trm.position, rot);
+        return Instantiate(obj, trm.position, rot);
     }
+    public void DestroyObj(GameObject obj)
+    {
+        Destroy(obj);
+    }
+        
 }
--- Assets/01.Script/4.Minyoung/Enemy/Boss/BulletBossPattern/StraightBullet.cs
+++ Assets/01.Script/4.Minyoung/Enemy/Boss/BulletBossPattern/StraightBullet.cs
@@ -7,28 +7,47 @@ public class StraightBullet : BossState<BulletBoss>
     private BulletBoss bulletBoss;
     public override void Enter()
     {
+        Debug.Log("직성공격시작");
+        bulletBoss = stateMachineOwnerClass as BulletBoss;
+        stateMachineOwnerClass.StartCoroutine(StarightAtk());
         //공격 애니메이션 실행
-        StarightAtk();
     }
 
     public override void Execute()
     {
+        bulletBoss.LookTarget(); //보스가 플레이어를 바라본다
 
     }
-        
+
     IEnumerator StarightAtk()
     {
-        bulletBoss?.LookTarget(); //보스가 플레이어를 바라본다
-        GameObject newBullet = bulletBoss.bullet; //불렛 생성 근데 인스턴티에이트가 안됨
-        for (int i = 0; i < 4; i++)
+        Debug.Log("일자어택함");
+        for (int i = 0; i < 3; i++)
         {
-            stateMachineOwnerClass.InstantiateObj(bulletBoss.bullet, stateMachineOwnerClass.transform, Quaternion.identity);
-            yield return new WaitForSeconds(0.5f);
+            for (int j = 0; j < 4; j++)
+            {
+                bulletBoss.LookTarget(); //보스가 플레이어를 바라본다
+                GameObject newbullet = stateMachineOwnerClass.InstantiateObj
+                    (bulletBoss.bullet, stateMachineOwnerClass.transform, Quaternion.identity);
+
+                newbullet.transform.rotation = Quaternion.LookRotation
+                    (stateMachineOwnerClass.Target.position - stateMachineOwnerClass.transform.position);
+
+                //newbullet.GetComponent<goFoward>().A();
+                Rigidbody rigid = newbullet.GetComponent<Rigidbody>();
+                rigid.AddForce(newbullet.transform.forward * 7f, ForceMode.Impulse);
+
+                yield return new WaitForSeconds(0.5f);
+            }
+            yield return new WaitForSeconds(1f);
         }
+        //GameObject newBullet = bulletBoss.bullet; //불렛 생성 근데 인스턴티에이트가 안됨
+
         //스타트코루틴 들어가자마자 하고
         // 불렛 따라 so만들어서 스크립트붙여서 update에서 foward방향 이동
-        yield return new WaitForSeconds(1f);
         //불렛을 생성한다
+        yield return new WaitForSeconds(1f);
+        stateMachine.ChangeState<CircleBullet>();
     }
     public override void Exit()
     {
