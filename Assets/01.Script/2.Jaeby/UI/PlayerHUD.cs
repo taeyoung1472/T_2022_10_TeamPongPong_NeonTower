38ab06e0ed892221d814e343a7aa3ebf7d76926e
myidmm64 <72392141+myidmm64@users.noreply.github.com>
2022-10-19 01:12:43 +0900

    1019_HUD 개발 완료

--- Assets/01.Script/2.Jaeby/UI/PlayerHUD.cs
+++ Assets/01.Script/2.Jaeby/UI/PlayerHUD.cs
@@ -1,18 +1,83 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHUD : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField]
+    private Slider _hpSlider = null;
+
+    private int _hpMaxValue = 0;
+    public int HPMaxValue
     {
-        
+        get => _hpMaxValue;
+        set
+        {
+            _hpMaxValue = value;
+            _hpSlider.maxValue = _hpMaxValue;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private int _hpMinValue = 0;
+    public int HPMinValue
     {
-        
+        get => _hpMinValue;
+        set
+        {
+            _hpMinValue = value;
+            _hpSlider.minValue = _hpMinValue;
+        }
+    }
+
+    private int _hpValue = 0;
+    public int HPValue
+    {
+        get => _hpValue;
+        set
+        {
+            _hpValue = value;
+            _hpSlider.value = _hpValue;
+        }
+    }
+
+    [SerializeField]
+    private TextMeshProUGUI _dashText = null;
+    [SerializeField]
+    private Color _EnableColor = Color.white;
+    [SerializeField]
+    private Color _DisableColor = Color.white;
+
+    public void SetDashValue(int value, int maxValue)
+    {
+        if (value < 0) return;
+
+        print($"Value : {value}, MaxValue : {maxValue}");
+
+        string str = "";
+        str += $"<#{ColorUtility.ToHtmlStringRGBA(_EnableColor)}>";
+        for (int i = value; i < maxValue; i++)
+        {
+            str += "¢Â ";
+        }
+        str += "</color>";
+
+        str += $"<#{ColorUtility.ToHtmlStringRGBA(_DisableColor)}>";
+        for (int i = 0; i < value; i++)
+        {
+            str += "¢Â ";
+        }
+        str += "</color>";
+
+        _dashText.text = str;
+    }
+
+    public void HpSliderInit(int minValue, int maxValue, int Value)
+    {
+        HPMinValue = minValue;
+        HPMaxValue = maxValue;
+        HPValue = Value;
     }
 }
