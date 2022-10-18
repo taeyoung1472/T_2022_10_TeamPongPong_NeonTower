5555c37c446b7521aad9d771c1600371fcd7eb5c
myidmm64 <72392141+myidmm64@users.noreply.github.com>
2022-10-19 00:06:32 +0900

    1019_웨이브 어택 제작

--- Assets/01.Script/1.Taeyoung/Enemy/Boss/RushBoss/Pattern/WaveAttack_RushBoss.cs
+++ Assets/01.Script/1.Taeyoung/Enemy/Boss/RushBoss/Pattern/WaveAttack_RushBoss.cs
@@ -7,6 +7,33 @@ public class WaveAttack_RushBoss<T> : BossState<RushBoss> where T : BossBase<T>
 {
     public override void Enter()
     {
+        stateMachineOwnerClass.transform.LookAt(stateMachineOwnerClass.Target.position);
+        stateMachineOwnerClass.StartCoroutine(WaveCoroutine());
+    }
+
+    private IEnumerator WaveCoroutine()
+    {
+        float size = 4f;
+        Vector3 plus = stateMachineOwnerClass.transform.forward;
+
+        DangerZone.DrawCircle(stateMachineOwnerClass.transform.position + plus * 2f, 4f, 1f);
+        yield return new WaitForSeconds(0.1f);
+        DangerZone.DrawCircle(stateMachineOwnerClass.transform.position + plus * 2f + plus * size, 4f, 1f);
+        yield return new WaitForSeconds(0.1f);
+        size += 4f;
+        DangerZone.DrawCircle(stateMachineOwnerClass.transform.position + plus * 2f + plus * size, 4f, 1f);
+        yield return new WaitForSeconds(0.1f);
+        size += 4f;
+        DangerZone.DrawCircle(stateMachineOwnerClass.transform.position + plus * 2f + plus * size, 4f, 1f);
+        yield return new WaitForSeconds(0.3f);
+
+        stateMachineOwnerClass.Animator.Play("GroundPound");
+        stateMachineOwnerClass.Animator.Update(0);
+        yield return new WaitUntil(() => stateMachineOwnerClass.Animator.GetCurrentAnimatorStateInfo(0).IsName("GroundPound") == false);
+        CameraManager.Instance.CameraShake(12f, 30f, 0.23f);
+
+        yield return new WaitForSeconds(1f);
+        stateMachine.ChangeState<Idle_RushBoss<RushBoss>>();
     }
 
     public override void Execute()
