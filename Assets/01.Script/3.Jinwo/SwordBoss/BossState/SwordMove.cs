fd0dbddf3ef2685de5313948811711060ae34f31
jinwoo0427 <yjw7770427@gmail.com>
2022-10-22 20:31:14 +0900

    이이잉

--- Assets/01.Script/3.Jinwo/SwordBoss/BossState/SwordMove.cs
+++ Assets/01.Script/3.Jinwo/SwordBoss/BossState/SwordMove.cs
@@ -70,16 +70,16 @@ public class SwordMove<T> : BossState<T> where T : Sword
         switch (a)
         {
             case 1:
-                DangerZone.DrawCircle(dangerzonePos, stateMachineOwnerClass.radius[a - 1] * 2, 1.5f); // 외곡 찍기
+                DangerZone.DrawCircle(dangerzonePos, stateMachineOwnerClass.radius[a - 1] * 2 /5, 1.5f); // 외곡 찍기
                 break;
             case 4:
-                DangerZone.DrawCircle(dangerzonePos, stateMachineOwnerClass.radius[a - 1] *2, 1.5f);  //연속베기 (콤보1)
+                DangerZone.DrawCircle(dangerzonePos, stateMachineOwnerClass.radius[a - 1] *2 /5, 1.5f);  //연속베기 (콤보1)
                 break;
             case 5:
-                DangerZone.DrawCircle(dangerzonePos, stateMachineOwnerClass.radius[a - 1] * 2, 1.5f);  //연속베기 (콤보2)
+                DangerZone.DrawCircle(dangerzonePos, stateMachineOwnerClass.radius[a - 1] * 2 / 5, 1.5f);  //연속베기 (콤보2)
                 break;
             case 6:
-                DangerZone.DrawCircle(dangerzonePos, stateMachineOwnerClass.radius[a - 1] * 2, 1.5f); //원 범위 공격 ( 텔, 대쉬해서 공격)
+                DangerZone.DrawCircle(dangerzonePos, stateMachineOwnerClass.radius[a - 1] * 2 / 5, 1.5f); //원 범위 공격 ( 텔, 대쉬해서 공격)
                 break;
 
         }
@@ -103,15 +103,15 @@ public class SwordMove<T> : BossState<T> where T : Sword
         switch (a)
         {
             case 2:
-                DangerZone.DrawArc(dangerzonePos, d, 5, new Vector3(2, 0.1f, 8), 1.25f); // 내려찍기
+                DangerZone.DrawArc(dangerzonePos, d, 5, new Vector3(2, 0.1f, 8) /5, 1.25f); // 내려찍기
                 stateMachineOwnerClass.arcangle = 15f;
                 break;
             case 3:
-                DangerZone.DrawArc(dangerzonePos, d, 4, new Vector3(3.5f, 0.1f, 14), 1.75f); //회오리 참격베기
+                DangerZone.DrawArc(dangerzonePos, d, 4, new Vector3(3.5f, 0.1f, 14)/5, 1.75f); //회오리 참격베기
                 stateMachineOwnerClass.arcangle = 18f;
                 break;
             case 7:
-                DangerZone.DrawArc(dangerzonePos, d, 2, new Vector3(7, 0.1f, 10), 1.75f); //발도 기술
+                DangerZone.DrawArc(dangerzonePos, d, 2, new Vector3(7, 0.1f, 10) / 5, 1.75f); //발도 기술
                 stateMachineOwnerClass.arcangle = 40f;
                 break;
         }
